308c551be0609db0178ee2259f9c3599834ca706
Nicolas Buehringer <n.buehringer@gmail.com>
2021-08-31 13:15:32 +0200

    changed to be able to run on gcr

--- project_delphi/twitter_api_data.py
+++ project_delphi/twitter_api_data.py
@@ -1,15 +1,17 @@
+from project_delphi.utils import get_date_n_days_ago
 import requests
 import time
 import datetime
 import os
 import pandas as pd
-from google.cloud import storage
 from project_delphi.twitter_api_params import query_dict, get_credentials
 from project_delphi.params import BUCKET_NAME
 
 
 def create_url(keywords, start_date, end_date, max_results = 10):
-    """Create the url with current keywords"""
+    """
+    Create the url with current keywords
+    """
 
     #Change to the endpoint you want to collect data from
     search_url = "https://api.twitter.com/2/tweets/search/all"
@@ -30,7 +32,9 @@ def create_url(keywords, start_date, end_date, max_results = 10):
 
 
 def connect_to_endpoint(url, headers, params, next_token):
-    """Returns the api response in a json format and sets 'next_token' value"""
+    """
+    Returns the api response in a json format and sets 'next_token' value
+    """
 
     #params dict received from create_url function, set next_token value
     params['next_token'] = next_token
@@ -51,7 +55,9 @@ def call_api(headers,
              end_time,
              max_results,
              new_token=None):
-    """Calls the twitter api and returns a dataframe containig all fetched information"""
+    """
+    Calls the twitter api and returns a dataframe containig all fetched information
+    """
 
     # create url by calling above defined function; url is a tuple
     url = create_url(keywords, start_time, end_time, max_results)
@@ -92,11 +98,13 @@ def call_api(headers,
     return merged_df, next_token, collected_tweets
 
 
-def get_data(party, keywords, start_time,
+def search_twitter(party, keywords, start_time,
              end_time, max_results, tweet_amount):
-    """Calls the twitter api until a maximun 'tweet_amount' has been reached or
-       there are no more results and next_token is False.
-       Returns a DataFrame containing all fethed information for one combination of keywords"""
+    """
+    Searches twitter for a keywords searchstring until a maximum 'tweet_amount' has been reached or
+    there are no more results and next_token is False.
+    Returns a DataFrame containing all fetched information
+    """
 
     # create a main DataFrame to which all the api call results for one party are concatenated
     one_party_df = pd.DataFrame()
@@ -146,52 +154,22 @@ def get_data(party, keywords, start_time,
     # return a DataFrame with all tweets for one time period
     return one_party_df
 
-
-
-"""def run_app():
-    temp_data = get_tweets
-
-    full_data = sentiment(temp_data)
-
-    cleaned_data = cleaner(full_data)"""
-
-
-def upload_tweets_to_gcp(date):
-
-    os.environ[
-        "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/nicolas/code/NicolasBuehringer/gcp/project-delphi-323909-05dee7633cbe.json"
-
-    client = storage.Client()
-
-    STORAGE_LOCATION = f"twitter_data/temp_tweets/temp_tweet_database_{date[5:7]}_{date[8:10]}.csv"
-
-    bucket = client.bucket(BUCKET_NAME)
-
-    blob = bucket.blob(STORAGE_LOCATION)
-
-    blob.upload_from_filename(
-        f"temp_tweet_database_{date[5:7]}_{date[8:10]}.csv"
-    )
-
-
-if __name__ == "__main__":
+def get_data(query_dict=query_dict):
     """
+    Returns a DataFrame containing all tweets for one day for 7 diffrent search keywords
+    for each party defined in query_dict
+    """
+
     # create a master DataFrame for all parties
     all_parties_df = pd.DataFrame()
 
-    # get current time"""
-    current_time = datetime.datetime.today()
-    """
-    # get current time yesterday
-    current_time_yesterday = (current_time - datetime.timedelta(1))
-
-    # convert to ISO 8601: YYYY-MM-DDTHH:mm:ssZ
-    # this is UTC; we are not accounting for german time zone +02:00
-    start_time = f"{str(current_time_yesterday)[:10]}T00:00:01.000Z"
-    end_time = f"{str(current_time)[:10]}T00:00:01.000Z"
-
     # set max results per api call
     max_results = 500
+
+    # convert to ISO 8601: YYYY-MM-DDTHH:mm:sssZ
+    # this is UTC; we are not accounting for german time zone +02:00
+    start_time = f"{get_date_n_days_ago(1).replace('_', '-')}T00:00:01.000Z"
+    end_time = f"{get_date_n_days_ago().replace('_', '-')}T00:00:01.000Z"
 
     # for key, value in query_dict from twitter_api_params.py
     for party, keywords in query_dict.items():
@@ -200,15 +178,21 @@ if __name__ == "__main__":
         tweet_amount =  keywords[1]
 
         # collect the DataFrame for one party-keyword combination
-        one_party_df = get_data(party, keywords[0], start_time,
+        one_party_df = search_twitter(party, keywords[0], start_time,
                              end_time, max_results, tweet_amount)
 
         # concat to master DataFrame
         all_parties_df = pd.concat([all_parties_df, one_party_df], ignore_index=True)
 
-        # save master DataFrame as a csv
-        all_parties_df.to_csv(
-            f"temp_tweet_database_{str(current_time)[5:7]}_{str(current_time)[8:10]}.csv"
-        )
-    """
-    upload_tweets_to_gcp(str(current_time))
+    # save master DataFrame as a csv
+    #all_parties_df.to_csv(
+    #    f"temp_tweet_database_{str(current_time_yesterday)[5:7]}_{str(current_time_yesterday)[8:10]}.csv"
+    #)
+
+
+    return all_parties_df
+
+
+
+if __name__ == "__main__":
+    pass
