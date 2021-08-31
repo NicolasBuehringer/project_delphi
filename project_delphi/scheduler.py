4975dc964fe72b9a2170528de12725bde0581ef3
Nicolas Buehringer <n.buehringer@gmail.com>
2021-08-31 13:14:18 +0200

    function that gets called through post api and runs whole app

---
+++ project_delphi/scheduler.py
@@ -0,0 +1,24 @@
+from project_delphi.merge_database import merge_daily_to_master
+from project_delphi.twitter_api_data import get_data
+from project_delphi.sentiment_analyzer import get_sentiment
+# from features.py import clean_database
+# run every day after midnight
+
+def run_app():
+    """
+    1. Collects all tweets for the last day for every party and stores in raw_tweets
+    2. Analyzes sentiment of daily_tweets and saves in daily_database
+    3. download old tweet database, concats new daily database, uploads it to gcs and saves it in tweet_database
+    """
+
+    daily_raw_tweets = get_data()
+
+    daily_database = get_sentiment(daily_raw_tweets)
+
+    tweet_database = merge_daily_to_master(daily_database)
+
+    # features_database = clean_database(tweet_database) --> Finn
+    # get_features()
+    # model = run_model(features_database)
+
+    # update api
